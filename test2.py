de69bcb9c093350d0731ecacdae80087e9808eb9
GenyoNguyen <huydragon234@gmail.com>
2023-09-21 23:54:50 +0700

    Edited test2.py to solve Gui tiet kiem

--- test2.py
+++ test2.py
@@ -1,2 +1,9 @@
-x = input()
-print(isinstance(x, int))
+x = []
+while len(x) < 3:
+    try:
+        temp = input().split()
+        for i in temp:
+            x.append(float(i))
+    except (ValueError, EOFError):
+        continue
+print(int(x[0]+(x[0]*x[2]/100)/12*x[1]))
